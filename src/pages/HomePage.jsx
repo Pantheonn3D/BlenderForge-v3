99814a18550968454bb19038f7279c6b5439ae66
Pantheonn3D <pantheon3d.contact@gmail.com>
2025-07-22 06:29:43 +0200

    Debug stats service and temporarily enable

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
-// src/pages/HomePage.jsx - Temporary test
+// src/pages/HomePage.jsx
 
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-// import StatsSection from '../components/StatsSection/StatsSection'; // Comment out
+import StatsSection from '../components/StatsSection/StatsSection';
 import ArticlesSection from '../components/ArticlesSection/ArticlesSection';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      {/* <StatsSection /> */} {/* Comment out temporarily */}
+      <StatsSection />
       <ArticlesSection />
     </>
   );
