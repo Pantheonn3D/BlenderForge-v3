509e623f7564ebb2df648137b96f8b6e1ee2a9e6
Pantheonn3D <pantheon3d.contact@gmail.com>
2025-07-22 06:24:16 +0200

    Debug stats service and temporarily disable StatsSection

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
-// src/pages/HomePage.jsx
+// src/pages/HomePage.jsx - Temporary test
 
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-import StatsSection from '../components/StatsSection/StatsSection';
+// import StatsSection from '../components/StatsSection/StatsSection'; // Comment out
 import ArticlesSection from '../components/ArticlesSection/ArticlesSection';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <StatsSection />
+      {/* <StatsSection /> */} {/* Comment out temporarily */}
       <ArticlesSection />
     </>
   );
